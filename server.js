be183c5765144df4ac1f4ae7f805e2c037d79187
Brandon <brandon@cupocode.com>
2023-04-06 22:00:08 -0400

    fixing api call for checkout

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.post('/api/login', async (req, res) => {
     });
   });
   
-  app.post('api/create-checkout-session', (req, res) => {
+  app.post('/api/create-checkout-session', (req, res) => {
     const socket = new WebSocket('ws://sea-turtle-app-l7rbe.ondigitalocean.app/create-checkout-session');
     socket.on('open', () => {
       socket.send('create session');
