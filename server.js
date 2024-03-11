c8a0751a34163245bc72e83fb8af397370882c5e
Brandon <brandon@cupocode.com>
2024-03-11 19:02:10 -0400

    fix

--- server.js
+++ server.js
@@ -169,13 +169,14 @@ app.get('/api/user/:userId', async (req, res) => {
 //     }
 // });
 
-app.post('/api/test', (req, res) => {
-    console.log('Received the followingpayload:', req.body); // Log the received payload
-    res.status(200).json({ message: 'Payload received successfully', receivedPayload: req.body });
-  });
+// app.post('/api/test', (req, res) => {
+//     console.log('Received the followingpayload:', req.body); // Log the received payload
+//     res.status(200).json({ message: 'Payload received successfully', receivedPayload: req.body });
+//   });
 
 
-  app.post('/wh-stripe', express.raw({type: 'application/json'}), async (req, res) => {
+  app.post('/api/wh-stripe', express.raw({type: 'application/json'}), async (req, res) => {
+    console.log('Received webhook event:');
     try {
         const event = JSON.parse(req.body);
 
